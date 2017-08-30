a5cd227b362e46a20c428da1c7f5a8357d74aa81
root <daryl.limkz@gmail.com>
2017-08-30 09:36:44 +0800

    Fixed xinetd buffering issues

--- Bashing/service/bash.py
+++ Bashing/service/bash.py
@@ -2,53 +2,64 @@
 
 import re
 import os
+import sys
 import random
 
+out = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+def output(string,end='\n'):
+	sys.stdout = out.write(string+end)
+
+def input(string=''):
+	output(string,end='')
+	value = raw_input()
+	return value
+
 def nachos():
-	print '''  _   _            _               
+	output('''  _   _            _               
  | \ | |          | |              
  |  \| | __ _  ___| |__   ___  ___ 
  | . ` |/ _` |/ __| '_ \ / _ \/ __|
  | |\  | (_| | (__| | | | (_) \__ \\
  |_| \_|\__,_|\___|_| |_|\___/|___/
-'''
+''')
 
 def fish():
-	print ''' ______ _     _     _           
+	output(''' ______ _     _     _           
 |  ____(_)   | |   (_)          
 | |__   _ ___| |__  _  ___  ___ 
 |  __| | / __| '_ \| |/ _ \/ __|
 | |    | \__ \ | | | |  __/\__ \\
 |_|    |_|___/_| |_|_|\___||___/
-'''
+''')
 
 def getFlag():
-	input = raw_input('''Foods available
+	value = input('''Foods available
 [n]achos
 [f]ishies
 > ''')
-	if (re.findall('[0-9a-eg-mo-zA-Z]',input) == []):
-		if input == 'n':
+	if (re.findall('[0-9a-eg-mo-zA-Z]',value) == []):
+		if value == 'n':
 			nachos()
-		elif input == 'f':
+		elif value == 'f':
 			fish()
-		print 'Hope you enjoy!'
-		print os.popen(input).read()
+		output('Hope you enjoy!')
+		output(os.popen(value).read())
 	else:
-		print 'Sorry, but we do not serve these here. Maybe next time!'
+		output('Sorry, but we do not serve these here. Maybe next time!')
 
 def main():
 	try:
-		input = raw_input('I am a [p]erson, [h]ungry > ')
-		if input == 'p':
-			print 'Hai human person!'
-		elif input == 'h':
+		value = input('I am a [p]erson, [h]ungry > ')
+		if value == 'p':
+			output('Hai human person!')
+		elif value == 'h':
 			getFlag()
 		else:
-			print 'Bye!'
+			output('Bye!')
 			return
 	except:
-		print 'See you again soon!'
+		output('See you again soon!')
 
 if __name__ == '__main__':
 	main()
