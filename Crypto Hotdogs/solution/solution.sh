9ecc6a0a2197477d6979ecb93cb43a832f784033
root <daryl.limkz@gmail.com>
2017-09-02 21:12:20 +0800

    Updated solution

--- Crypto Hotdogs/solution/solution.sh
+++ Crypto Hotdogs/solution/solution.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+openssl rsa -pubin -text -noout -in public.pem
 python3 wiener_attack/RSAwienerHacker.py
 python solution.py
 openssl rsautl -decrypt -in flag.bin -inkey private.pem -out flag.txt
