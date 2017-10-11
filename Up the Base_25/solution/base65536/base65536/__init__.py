50cf721a76688faf37c0b7a1d32453d293e817ab
root <daryl.limkz@gmail.com>
2017-10-11 09:21:27 +0800

    Update

---
+++ Up the Base_25/solution/base65536/base65536/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+   base65536
+   ~~~~~~~~~
+
+   base65536 Python implementation.
+
+"""
+from .__about__ import __version__  # noqa
+from .core import decode, encode
+
+
+__all__ = ['encode', 'decode']
